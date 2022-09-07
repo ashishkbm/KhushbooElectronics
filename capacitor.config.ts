28ad35255092e7481336bf5ff7e31fb16e479ad0
ashishkbm <84813982+ashishkbm@users.noreply.github.com>
2022-09-08 02:01:50 +0530

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.jamesonsaunders.convertwebsitetoapp',
-  appName: 'Website to App',
+  appId: 'com.anosdigital',
+  appName: 'ANOS DIGTTAL,
   webDir: 'www',
   bundledWebRuntime: false,
 };
